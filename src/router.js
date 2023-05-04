684c26f01163e513bb8a8b374c84b3796cf7ba98
YEMINOO <pudding1387@gmail.com>
2023-05-04 19:15:46 +0630

    add auth template

--- src/router.js
+++ src/router.js
@@ -1,10 +1,34 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
+import App from "App";
+import AuthLayout from "layouts/AuthLayout";
+import GuestLayout from "layouts/GuestLayout";
+import Login from "components/auth/Login";
+import Register from "components/auth/Register";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
-		element: <App />,
+		element: <AuthLayout />,
+		children: [
+			{
+				path: "/",
+				element: <App />
+			}
+		]
+	},
+	{
+		path: "/",
+		element: <GuestLayout />,
+		children: [
+			{
+				path: "login",
+				element: <Login />
+			},
+			{
+				path: "register",
+				element: <Register />
+			}
+		]
 	},
 ]);
 
