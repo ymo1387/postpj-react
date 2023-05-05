d321e19ee52296989bbffe5af9abdd9e8ef1e6fa
YEMINOO <pudding1387@gmail.com>
2023-05-06 00:42:32 +0630

    fix nav item name

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -27,7 +27,10 @@ const NavBar = () => {
 						<div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
 							<NavItem path="/home" text="Home" />
 							<NavItem path="/myposts" text="My Posts" />
-							<NavItem path="/user/subscribers" text="My Posts" />
+							<NavItem
+								path="/user/subscribers"
+								text="Subscribers"
+							/>
 						</div>
 					</div>
 					<div className="flex">
