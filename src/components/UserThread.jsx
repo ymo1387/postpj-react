6ea3d6bf848769b3a2261f8e195902f0831dd4cd
YEMINOO <pudding1387@gmail.com>
2023-05-06 19:23:59 +0630

    fix subscribe function

--- src/components/UserThread.jsx
+++ src/components/UserThread.jsx
@@ -6,12 +6,12 @@ const UserThread = (props) => {
 	const [disable, setDisable] = useState(false);
 	const [isSub, setIsSub] = useState(sub);
 
-	const subscribe = () => {
+	const subfn = (path, _isSub) => {
 		setDisable(true);
 		axiosToken
-			.post(`/v1/users/${user.id}/subscribe`)
+			.post(path)
 			.then((res) => {
-				setIsSub(true);
+				setIsSub(_isSub);
 				setDisable(false);
 			})
 			.catch((err) => {
@@ -20,13 +20,29 @@ const UserThread = (props) => {
 			});
 	};
 
+	const subscribe = () => {
+		subfn(`/v1/users/${user.id}/subscribe`, true);
+	};
+
+	const unsubscribe = () => {
+		subfn(`/v1/users/${user.id}/unsubscribe`, false);
+	};
+
 	return (
 		<div className="shadow-md p-3 m-3 flex justify-between">
 			<div>
 				<h3>{user.name}</h3>
 			</div>
 			<div>
-				{!isSub && (
+				{isSub ? (
+					<button
+						onClick={unsubscribe}
+						className="bg-transparent hover:bg-red-500 text-red-500 font-semibold hover:text-white py-1 px-2 border border-red-500 hover:border-transparent rounded disabled:bg-gray-400 disabled:text-slate-700 disabled:cursor-no-drop"
+						disabled={disable}
+					>
+						Unsubscribe
+					</button>
+				) : (
 					<button
 						onClick={subscribe}
 						className="bg-transparent hover:bg-blue-500 text-blue-500 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded disabled:bg-gray-400 disabled:text-slate-700 disabled:cursor-no-drop"
--- src/pages/Subscribers.jsx
+++ src/pages/Subscribers.jsx
@@ -1,40 +1,6 @@
 import { axiosToken } from "axios-client";
+import UserThread from "components/UserThread";
 import { useEffect, useState } from "react";
-
-const SuberThread = ({ sub, toSuber, setToSuber }) => {
-	const [disable, setDisable] = useState(false);
-
-	const unsubscribe = () => {
-		setDisable(true);
-		axiosToken
-			.post(`/v1/users/${sub.id}/unsubscribe`)
-			.then((res) => {
-				setToSuber(toSuber.filter((old) => old.id !== sub.id));
-				setDisable(false);
-			})
-			.catch((err) => {
-				console.log(err);
-				setDisable(false);
-			});
-	};
-
-	return (
-		<div className="shadow-md p-3 m-3 flex justify-between">
-			<div>
-				<h3>{sub.name}</h3>
-			</div>
-			<div>
-				<button
-					onClick={unsubscribe}
-					className="bg-transparent hover:bg-red-500 text-red-500 font-semibold hover:text-white py-1 px-2 border border-red-500 hover:border-transparent rounded disabled:bg-gray-400 disabled:text-slate-700 disabled:cursor-no-drop"
-					disabled={disable}
-				>
-					Unsubscribe
-				</button>
-			</div>
-		</div>
-	);
-};
 
 export default function Subscribers() {
 	const [toSuber, setToSuber] = useState([]);
@@ -68,11 +34,10 @@ export default function Subscribers() {
 					{toSuber.length > 0 ? (
 						<>
 							{toSuber.map((sub) => (
-								<SuberThread
+								<UserThread
 									key={sub.id}
-									sub={sub}
-									toSuber={toSuber}
-									setToSuber={setToSuber}
+									user={sub}
+									sub={true}
 								/>
 							))}
 						</>
